f76dcbc2c59336ddce196b618656b8d9f91f0078
ukc10014 <44382449+ukc10014@users.noreply.github.com>
2024-07-15 19:50:37 +0100

    Initial commit

---
+++ src/components/Header.svelte
@@ -0,0 +1,29 @@
+<script>
+	import wordmark from "$svg/wordmark-shadow.svg";
+</script>
+
+<header>
+	<div class="wordmark">
+		<a href="https://pudding.cool" aria-label="The Pudding" target="_self"
+			>{@html wordmark}</a
+		>
+	</div>
+</header>
+
+<style>
+	.wordmark {
+		max-width: 10em;
+		margin: 0 auto;
+		padding: 1em 0;
+	}
+
+	.wordmark a {
+		border: none;
+		display: block;
+		color: var(--color-fg);
+	}
+
+	.wordmark a:hover {
+		background-color: transparent;
+	}
+</style>
